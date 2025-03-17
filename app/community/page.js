4a6162c1fc839c5f0f37e0cee1ac02d7308e3ae9
Oludire Damilare <oludire@gmail.com>
2025-03-17 17:21:40 +0100

    first commit

---
+++ app/community/page.js
@@ -0,0 +1,5 @@
+function CommunityPage() {
+  return <h1>community</h1>;
+}
+
+export default CommunityPage;
---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,5 @@
+function MealDetailPage() {
+  return <h1>Meal Details</h1>;
+}
+
+export default MealDetailPage;
