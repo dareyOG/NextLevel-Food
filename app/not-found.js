b3374848a9ad7d0b310ee57afc42034b7d1e5d31
Oludire Damilare <oludire@gmail.com>
2025-04-04 03:23:03 +0100

    ✅handle not-found states

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+function PageNotFound() {
+    return (
+        <main className="not-found">
+            <h1>Page not found</h1>
+            <p>Unfortunately, we cannot find the requested page or resource.</p>
+        </main>
+    )
+}
+
+export default PageNotFound
