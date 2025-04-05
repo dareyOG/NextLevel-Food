5db7cb52b9409fed9b5f5e18b73e42384b799e44
Oludire Damilare <oludire@gmail.com>
2025-04-05 11:28:04 +0100

    ✅create server actions for handling form submission

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,23 +1,8 @@
 import ImagePicker from '@/components/meals/image-picker';
 import style from './page.module.css'
+import { shareMeal } from '@/lib/actions';
 
 function ShareMealPage() {
-
-  const shareMeal = async (formData) => {
-    'use server'
-
-    const meal = {
-      title: formData.get('title'),
-      summary: formData.get('summary'),
-      instructions: formData.get('instructions'),
-      image: formData.get('image'),
-      creator: formData.get('name'),
-      creator_email: formData.get('email'),
-    }
-
-    console.log(meal);
-
-  }
 
   return <>
     <header className={style.header}>
